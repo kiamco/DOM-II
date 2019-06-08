5bc6147b7dd9f6104397caf4240a1ce39e89914c
kiamco <kiamcoo@gmail.com>
2019-06-07 22:28:28 -0700

    working on mvp

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+const nav = document.querySelector('header');
+
+nav.addEventListener('mouseover', event => {
+    nav.style.background = 'pink';
+});
+
+nav.addEventListener('mouseleave', event => {
+    nav.style.background = 'white';
+});
+
+const btn = document.querySelectorAll('.btn')
+btn[0].addEventListener('click', event => {
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
