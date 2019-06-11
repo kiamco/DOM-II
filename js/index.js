fbd4717332f2e19d8641931e0c93e493562492ed
kiamco <kiamcoo@gmail.com>
2019-06-10 17:11:38 -0700

    completed mvp

--- js/index.js
+++ js/index.js
@@ -9,12 +9,24 @@ const btn = document.querySelectorAll('.btn')
 const close = document.querySelector('.close-btn');
 const modal = document.querySelector('.modal-content');
 const email = document.querySelector('.email');
+const welcomeP = document.querySelector('p');
+const firstImg = document.querySelector('img');
+const extra = document.querySelector('.extra');
+const extra2 = document.querySelector('.extra2')
 
 
 // header hover mouseover
 nav.addEventListener('mouseover', event => nav.style.background = 'pink');
 nav.addEventListener('mouseleave', event => nav.style.background = 'white');
 
+// wheel
+modal.addEventListener('wheel', event => {
+    if (modal.style.color === "black") {
+        modal.style.color = 'pink';
+    } else {
+        modal.style.color = 'black';
+    }
+});
 // show modal when submit click
 btn[0].addEventListener('click', event => {
     document.querySelector('.bg-modal').style.display = 'flex';
@@ -64,3 +76,30 @@ window.addEventListener('resize', event => {
     console.log(`browser height: ${event.target.innerHeight}`);
     console.log(`browser width: ${event.target.innerHeight}`);
 });
+
+// select
+email.addEventListener('select', event => {
+    email.style.fontSize = '4rem'
+});
+
+welcomeP.addEventListener('dblclick', event => welcomeP.textContent = 'Hello');
+
+// drag
+firstImg.addEventListener('drag', event => {
+    if (event !== null) {
+        firstImg.style.border = '2px solid black';
+    } else {
+        firstImg.style.border = 'none';
+    }
+
+});
+
+// stop prop
+extra2.addEventListener('click', event => {
+    event.stopPropagation();
+    alert('child')
+});
+extra.addEventListener('click', event => alert('parent'));
+
+// prevent defualt
+nav.addEventListener('click', event => event.preventDefault());
